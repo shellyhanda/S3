f4df3e1e88a96ffd04e396064b3157059e4eba99
Shelly Handa <shelly.handa@gmail.com>
2018-11-12 22:53:37 -0800

    S3

---
+++ workspace/ecsconnection/src/main/java/com/shelly/poc/ecs/ecsconnection/EcsconnectionApplication.java
@@ -0,0 +1,12 @@
+package com.shelly.poc.ecs.ecsconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsconnectionApplication.class, args);
+	}
+}
